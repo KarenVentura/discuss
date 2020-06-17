552a4fa6fc453e4d6a5414b23296916b29d1c882
Karen Ventura <akv802@gmail.com>
2020-06-17 00:16:54 -0500

    Use alias statement

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -2,17 +2,14 @@ defmodule DiscussWeb.TopicController do
   # discuss/lib/discuss_web.ex include all the functionality for controller
   # use is a fancy wey to import the funcitonality look at the file  talked before
   use DiscussWeb, :controller 
+  alias Discuss.Topic # with this we can use only Topic to call something
 
   def new(conn, params) do
     IO.puts "++++++"
     # inspect show every value of the data structure
     # conn is a struct represents the incoming request and the outgoing request, is as request in rails
-    IO.inspect conn
-    IO.puts "++++++"
-    IO.inspect params
-    #render(conn, "index.html")
-    struct = %Discuss.Topic{}
+    struct = %Topic{}
     params = %{}
-    changeset = Discuss.Topic.changeset(struct, params)
+    changeset = Topic.changeset(struct, params)
   end
 end
